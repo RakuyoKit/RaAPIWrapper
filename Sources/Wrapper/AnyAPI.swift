667e0d439a93a84c598618a9c31a9fb910c6efe9
rakuyo <rakuyo.mo@gmail.com>
2022-08-29 09:03:35 +0800

    [Add] add `AnyAPI `

---
+++ Sources/Wrapper/AnyAPI.swift
@@ -0,0 +1,45 @@
+//
+//  AnyAPI.swift
+//  RaAPIWrapper
+//
+//  Created by Rakuyo on 2022/8/28.
+//  Copyright © 2022 Rakuyo. All rights reserved.
+//
+
+import Foundation
+
+/// Generic Erasure for `API`.
+///
+/// You can use this type as a parameter or return value of a function
+/// to say "I just need an api and don't care what it is".
+///
+/// For example.
+///
+/// ```swift
+/// // In the parent class.
+/// func listAPI() -> AnyAPI? { nil }
+///
+/// // In subclass: View jobs for a particular situation
+/// override func listAPI() -> AnyAPI? { ListAPI.$jobs }
+///
+/// // In subclass: View jobs for a all situation
+/// override func listAPI() -> AnyAPI? { ListAPI.$allJobs }
+/// ```
+public final class AnyAPI: API<Any> {
+    public init<P>(_ api: API<P>) {
+        super.init(
+            wrappedValue: {
+                guard let builder = api.wrappedValue else { return nil }
+                return { builder($0 as! P) }
+            }(),
+            api.path,
+            decodeType: api.decodeType,
+            specialBaseURL: api.specialBaseURL,
+            method: api.method,
+            header: {
+                guard let builder = api.headerBuilder else { return nil }
+                return { builder($0 as! P) }
+            }(),
+            parameterEncoding: api.parameterEncoding)
+    }
+}
