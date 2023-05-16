35f1b8d278a4c04a0cd8984f6aa1749cb1cecb21
Rakuyo <rakuyo.mo@gmail.com>
2023-05-16 19:05:57 +0800

    [Update] Refine test cases

---
+++ Tests/TestEnum.swift
@@ -0,0 +1,14 @@
+//
+//  TestEnum.swift
+//  RaAPIWrapper
+//
+//  Created by Rakuyo on 2023/5/16.
+//  Copyright © 2022 Rakuyo. All rights reserved.
+//
+
+import Foundation
+
+enum TestEnum: Int {
+    case one = 1
+    case two = 2
+}
