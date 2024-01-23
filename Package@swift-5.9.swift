68dcebb7394d974cadceeccdec8fc87b91b4fd1a
Rakuyo <rakuyo.mo@gmail.com>
2024-01-23 15:18:44 +0800

    [Update] Add Package@swift-5.9

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "APIWrapper",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_14),
+        .tvOS(.v12),
+        .watchOS(.v5),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(name: "APIWrapper", targets: ["APIWrapper"]),
+        .library(name: "AFAPIWrapper", targets: ["AFAPIWrapper"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Alamofire/Alamofire.git",
+            .upToNextMajor(from: "5.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "APIWrapper",
+            path: "Sources/Core"),
+        .target(
+            name: "AFAPIWrapper",
+            dependencies: ["APIWrapper", "Alamofire"],
+            path: "Sources/Alamofire"),
+        .testTarget(
+            name: "APIWrapperTests",
+            dependencies: ["APIWrapper"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
